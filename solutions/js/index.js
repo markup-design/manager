78e04749660649e0bd294d48d6c4ded1705c72c9
nullsage <nullsage@gmail.com>
2016-01-07 20:19:33 -0500

    Refactoring code for solutions
    Solutions page now has the ability to output code straight from a file.
    See outputImportantDocs() method for details. Note: This method still
    needs documented.

--- solutions/js/index.js
+++ solutions/js/index.js
@@ -1,6 +1,34 @@
 $(document).ready(function () {
 	
-	$("#accordion").accordion();
+	$("#accordion").accordion({
+		heightStyle: "content"
+	});
 	
 	$("button").button();
+	
+	$(".raw-activate").on("click", function () {
+		
+		$(this).parent().find(".raw-documentation").toggleClass("display-none");
+	});
+	
+	/*
+	My attempt to make some sort of color scheme in the <pre> tag code block
+	
+	$(".raw-documentation pre").each(function () {
+		
+		var text = $(this).text();
+		
+		var newText = replaceAll(text, "function", "<div class='function'>function</div>");
+		
+		$(this).html(newText);
+	});*/
+	
+	function escapeRegExp(str) {
+		return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+	}
+	
+	function replaceAll(str, find, replace) {
+		
+	  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+	}
 });
