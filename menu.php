62719eb33b0b79c1dcceb0f286b50f638505f425
chewbacca385 <nate.baca.385@gmail.com>
2016-02-04 21:56:51 -0500

    Added link to login

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@
 		<li class='home mobile'><a href='/manager/'>Home</a></li>
 		<li><a href='/manager/solutions/'>Solutions</a></li>
 		<li><a href='/manager/docs/'>Documentation</a></li>
-		<li><a href='/manager/login/'>Login</a></li>
+		<li><a href='/manager/solutions/login/'>Login</a></li>
 		<li class='mobile'>
 			
 			<a href='#mobile-menu'>
