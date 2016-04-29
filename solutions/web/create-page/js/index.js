891fd5fc184af23945631157ea06452fa3c250cb
nullsage <nullsage@gmail.com>
2016-04-29 00:28:30 -0400

    Added more functionality to web page creator
    -can create index page and creates js and css folders with corresponding
    index files
    -if not index file, it just creates the file in the specified path

--- solutions/web/create-page/js/index.js
+++ solutions/web/create-page/js/index.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
 	
+	$("button").button();
+	
+	checkPageType();
+	
+	$("#page-type").on("change", function () {
+		
+		checkPageType();
+	});
+	
+	function checkPageType () {
+		
+		var value = $("#page-type").val();
+		
+		if (value === "index") {
+			
+			$("#page-name").val("index").parent().addClass("display-none");
+		}
+		else $("#page-name").val("").parent().removeClass("display-none");
+	};
 });
